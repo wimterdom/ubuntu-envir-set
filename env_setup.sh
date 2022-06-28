97327b7307fc22d3b21959f91832f64e3ea575a5
Siang <41909558+wimterdom@users.noreply.github.com>
2022-06-28 21:46:06 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -5,121 +5,121 @@ cd $HOME
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-#echo "Install 嘸蝦米..."
+#echo "[*]Install 嘸蝦米..."
 #sudo apt-get install fcitx fcitx-m17n -y
 #sudo apt-get install fcitx-table-boshiamy -y
-#echo "Install finished!"
+#echo "[*]Install finished!"
 #echo ""
 
-echo "Install net-tools..."
+echo "[*]Install net-tools..."
 sudo apt-get -y install net-tools
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install curl..."
+echo "[*]Install curl..."
 sudo apt install -y curl
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install ruby..."
+echo "[*]Install ruby..."
 sudo apt-get -y install ruby
 sudo apt-get -y install gcc ruby-dev
-echo "Install finished..."
+echo "[*]Install finished..."
 echo ""
 
-echo "Install Htop..."
+echo "[*]Install Htop..."
 sudo apt-get -y install htop
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install NCDU"
+echo "[*]Install NCDU"
 sudo apt-get -y install ncdu
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install tree"
+echo "[*]Install tree"
 sudo apt-get -y install tree
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install nautilus"
+echo "[*]Install nautilus"
 sudo apt-get -y install nautilus
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install gedit..."
+echo "[*]Install gedit..."
 sudo apt-get -y install gedit
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install sublime-text"
+echo "[*]Install sublime-text"
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
 sudo apt -y install sublime-text
-echo "Install finished!"
+echo "[*]Install finished!"
 
 
-echo "Install python3、python3-dev&pip..."
+echo "[*]Install python3、python3-dev&pip..."
 sudo apt-get -y install python3
 sudo apt-get -y install python3-dev
 sudo apt-get -y install python3-pip
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install python..."
+echo "[*]Install python..."
 sudo apt-get -y install python
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install Powerline..."
+echo "[*]Install Powerline..."
 sudo apt-get -y install powerline
-echo "Install finished!"
+echo "[*]Install finished!"
 
-echo "Install Vim..."
+echo "[*]Install Vim..."
 sudo apt-get -y install vim
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install tmux..."
+echo "[*]Install tmux..."
 sudo apt-get -y install tmux
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install virtualenv..."
+echo "[*]Install virtualenv..."
 sudo apt-get -y install python3-virtualenv
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install terminator..."
+echo "[*]Install terminator..."
 sudo apt-get -y install terminator
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install bat"
+echo "[*]Install bat"
 sudo apt-get -y install bat
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install snap..."
+echo "[*]Install snap..."
 sudo apt-get -y install snap
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install gotop..."
+echo "[*]Install gotop..."
 snap install gotop-cjbassi
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install Reverse software"
-echo "Install Radare2..."
+echo "[*]Install Reverse software"
+echo "[*]Install Radare2..."
 git clone https://github.com/radareorg/radare2
 cd radare2/sys/
 ./install.sh
-ehco "Install finished!"
+ehco "[*]Install finished!"
 echo ""
 
-echo "Install pwn software..."
+echo "[*]Install pwn software..."
 sudo gem install seccomp-tools
 sudo apt-get -y install libseccomp-dev
 sudo apt-get -y install binutils nasm
@@ -142,10 +142,10 @@ git clone https://github.com/JonathanSalwan/ROPgadget
 cd ROPgadget
 python setup.py install
 sudo pip3 install one_gadget
-echo "Install finished!"
+echo "[*]Install finished!"
 echo ""
 
-echo "Install Docker..."
+echo "[*]Install Docker..."
 sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -153,4 +153,4 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 sudo apt-get -y install docker-compose
 sudo systemctl status docker
-echo "Install finished!"
+echo "[*]Install finished!"
