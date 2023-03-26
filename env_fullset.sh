1eb2e86c991a64053c3502e8a90831ac032f482f
Siang <41909558+wimterdom@users.noreply.github.com>
2023-03-26 14:48:37 +0800

    Update env_fullset.sh

--- env_fullset.sh
+++ env_fullset.sh
@@ -186,7 +186,10 @@ echo "\033[31m [*] Install Wine... \033[0m"
 cat <<"EOF" | bash                              
 sudo dpkg --add-architecture i386 && \
 sudo apt update && \
-sudo apt install wine64 wine32 && \
+wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add - && \
+sudo apt install software-properties-common && \
+sudo apt-add-repository "deb http://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main" && \
+sudo apt install -y --install-recommends winehq-stable
 wine --version
 EOF
 echo "\033[32m [*] Wine install finished! \033[0m"
